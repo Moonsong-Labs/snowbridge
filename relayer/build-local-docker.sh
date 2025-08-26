bd7481fce08b43ab7071e9411f846ad1a2a838c9
Tobi Demeco <50408393+TDemeco@users.noreply.github.com>
2025-08-26 10:52:06 -0300

    fix: :bug: update Docker build platform (#20)
    This PR updates the platform used when building the relayer image with
    Docker to match what's expected on DataHaven (`linux/amd64`).

--- relayer/build-local-docker.sh
+++ relayer/build-local-docker.sh
@@ -5,7 +5,7 @@ source ../web/packages/test/scripts/set-env.sh
 
 build_image()
 {
-    docker build -f Dockerfile -t snowbridge-relay:local .
+    docker build --platform linux/amd64 -f Dockerfile -t snowbridge-relay:local .
 }
 
 if [ -z "${from_start_services:-}" ]; then
